9926e2b1c3b80f30a17068acb7d918fb51c3b0dc
dog-eared <evanmulrooney@gmail.com>
2019-12-09 06:32:46 -0500

    Redoing portfolio, again!

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const scrollOffset = 64;
+
+window.addEventListener("load", function() {
+    var nav = document.getElementById('navigation');
+    window.onscroll = function () { 
+        "use strict";
+        if (window.pageYOffset > scrollOffset || document.documentElement.scrollTop > scrollOffset || document.body.scrollTop >= scrollOffset ) {
+            nav.classList.add("nav--opaque");
+        } 
+        else {
+            nav.classList.remove("nav--opaque");
+        }
+    };
+});
+
