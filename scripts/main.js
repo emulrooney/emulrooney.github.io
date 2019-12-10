b248243d04c17917ce43bbe7cddc47ac7beed47f
dog-eared <evanmulrooney@gmail.com>
2019-12-10 17:50:06 -0500

    Tweak media query to better accommodate projects container

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ const scrollOffset = 64;
 
 window.addEventListener("load", function() {
     var nav = document.getElementById('navigation');
-    nav.classList.remove("nav--opaque");
 
     window.onscroll = function () { 
         "use strict";
@@ -13,5 +12,7 @@ window.addEventListener("load", function() {
             nav.classList.remove("nav--opaque");
         }
     };
+
+    window.onscroll();
 });
 
