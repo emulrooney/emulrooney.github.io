b5a51a57470382d4fb563c578dd1a1df3652ea5d
dog-eared <evanmulrooney@gmail.com>
2019-12-10 17:12:43 -0500

    Tweaked 'about' section styling for mobile, added navbar fallback for noScript

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ const scrollOffset = 64;
 
 window.addEventListener("load", function() {
     var nav = document.getElementById('navigation');
+    nav.classList.remove("nav--opaque");
+
     window.onscroll = function () { 
         "use strict";
         if (window.pageYOffset > scrollOffset || document.documentElement.scrollTop > scrollOffset || document.body.scrollTop >= scrollOffset ) {
